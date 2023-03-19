90117a4194e5b985994422b7d70178d8d8b51ecb
potat0777 <127588337+potat0777@users.noreply.github.com>
2023-03-19 21:52:58 +0200

    Add files via upload

---
+++ homew2.py
@@ -0,0 +1,7 @@
+class Cat:
+    def __init__(self):
+        self.name = "Barsik"
+        self.breed = "Britanets"
+        self.color = "Grey"
+    def GetFullInfo(self):
+        return f"Name :{self.name}\nBreed: {self.breed}\nColor: {self.color}"
